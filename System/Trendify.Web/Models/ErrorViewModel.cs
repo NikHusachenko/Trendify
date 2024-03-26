642c93bcfc34d76e3f9ba26b6d781a12a8ae526a
NikHusachenko <nikgusachenko@gmail.com>
2024-03-26 22:53:59 +0200

    Added system project group

---
+++ System/Trendify.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Trendify.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
