f2b450a50e7af1f7d817eeeb24f1b8560c080b01
NikHusachenko <nikgusachenko@gmail.com>
2024-04-03 21:12:00 +0300

    Added authenticaiton controller with login page

---
+++ System/Trendify.CuttingWorkshop.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trendify.CuttingWorkshop.Web.Controllers;
+
+public class AuthenticationController : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Login()
+    {
+        return View();
+    }
+}
