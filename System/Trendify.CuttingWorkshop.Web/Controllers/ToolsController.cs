f5245025e510412aaa5ec00e41c219da98ac3a9b
PavloSL12 <156653947+PavloSL12@users.noreply.github.com>
2024-04-10 20:17:54 +0300

    Added new controller

---
+++ System/Trendify.CuttingWorkshop.Web/Controllers/ToolsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trendify.CuttingWorkshop.Web.Controllers
+{
+    public class ToolsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Tools()
+        {
+            return View();
+        }
+    }
+}
