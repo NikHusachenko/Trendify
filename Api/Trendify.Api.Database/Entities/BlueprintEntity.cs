cb4b818299076083939a6761019709988e324743
Denys <162214741+adenchee@users.noreply.github.com>
2024-04-17 14:34:43 +0300

    Created new Entity for Blueprints

---
+++ Api/Trendify.Api.Database/Entities/BlueprintEntity.cs
@@ -0,0 +1,14 @@
+﻿using Trendify.Api.Database.Enums;
+
+namespace Trendify.Api.Database.Entities;
+
+public sealed record BlueprintEntity : BaseEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string BlueprintURL { get; set; }
+
+    public List<ProductEntity> Products { get; set; } = new List<ProductEntity>();
+    public List<MaterialEntity> Materials { get; set; } = new List<MaterialEntity>();
+}
