b875d79141d0c2609a810bd8d23493d680ec0567
PavloSL12 <156653947+PavloSL12@users.noreply.github.com>
2024-04-19 19:29:13 +0300

    Added Migration
    Note: changed localhost string to mine, change back to make it work

---
+++ Api/Trendify.Api.EntityFramework/Migrations/20240419162840_CreateInit.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trendify.Api.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BlueprintEntityId",
+                table: "Materials",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Blueprints",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    BlueprintURL = table.Column<string>(type: "text", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blueprints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Blueprints_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materials_BlueprintEntityId",
+                table: "Materials",
+                column: "BlueprintEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blueprints_ProductId",
+                table: "Blueprints",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Materials_Blueprints_BlueprintEntityId",
+                table: "Materials",
+                column: "BlueprintEntityId",
+                principalTable: "Blueprints",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Materials_Blueprints_BlueprintEntityId",
+                table: "Materials");
+
+            migrationBuilder.DropTable(
+                name: "Blueprints");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Materials_BlueprintEntityId",
+                table: "Materials");
+
+            migrationBuilder.DropColumn(
+                name: "BlueprintEntityId",
+                table: "Materials");
+        }
+    }
+}
