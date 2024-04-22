4474061b4aa17f0828f0c00652124c4b7544c60b
PavloSL12 <156653947+PavloSL12@users.noreply.github.com>
2024-04-22 20:40:36 +0300

    fixed storage for sewing

--- System/Trendify.SewingWorkshop.Web/Controllers/HomeController.cs
+++ System/Trendify.SewingWorkshop.Web/Controllers/HomeController.cs
@@ -36,6 +36,10 @@ namespace Trendify.SewingWorkshop.Web.Controllers
         {
             return View();
         }
+        public IActionResult Items()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
